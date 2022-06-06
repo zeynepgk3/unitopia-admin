3104550610bfe6bf3a7e283590b21a9db4098e43
zeynepgok <zeynep.gk3@gmail.com>
2022-06-06 03:05:24 +0300

    login and register

---
+++ js/xhr.js
@@ -0,0 +1,130 @@
+const registerbtn = document.getElementById('registerbtn');
+const loginbtn = document.getElementById('loginbtn');
+var loggedUser;
+
+console.log(registerbtn);
+const form = document.getElementById('registerForm');
+
+
+const sendHttpRequest = (method, url, data) => {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.responseType = 'json';
+
+    if (data) {
+      xhr.setRequestHeader('Content-Type', 'application/json');
+    }
+
+    xhr.onload = () => {
+      if (xhr.status >= 400) {
+        reject(xhr.response);
+      }
+      else {
+        resolve(xhr.response);
+      }
+      // const data = JSON.parse(xhr.response);
+      // console.log(data);
+    };
+
+    xhr.onerror = () => {
+      reject('Bir hata oldu!');
+    };
+
+    xhr.send(JSON.stringify(data));
+  });
+  return promise;
+};
+
+// const getUser = (email) => {
+//   console.log('email',email);
+//   fetch('http://localhost:3001/users/getOneByEmail', {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({
+//       email: email,
+//     })
+//   }).then(response => {
+//     console.log("responseeeeee");
+//     console.log(response);
+//     console.log("response222");
+//     console.log(response.json());
+//     loggedUser=response.json();
+//     console.log("loggedUser", loggedUser);
+//   });
+//   // location.assign('/index.html');
+// };
+
+const login = async () => {
+  const form = document.querySelector('form');
+  console.log('formEmail', form.email.value);
+  console.log('formPassword', form.password.value);
+
+  const data= await fetch('http://localhost:3001/users/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      email: form.email.value,
+      password: form.password.value
+    })
+  });
+  console.log("data: ",data);
+
+  loggedUser = await data.json();
+  console.log("loggedUser: ",loggedUser);
+
+  localStorage.setItem("name", loggedUser.name);
+  localStorage.setItem("role", loggedUser.role);
+  localStorage.setItem("email", loggedUser.email);
+
+  localStorage.getItem("name");
+  // location.assign('/index.html');
+};
+const logout = () => {
+  localStorage.setItem("name", null);
+  localStorage.setItem("role", null);
+  localStorage.setItem("email", null);
+
+  localStorage.getItem("name");
+  // location.assign('/index.html');
+};
+
+const register = () => {
+  const form = document.querySelector('form');
+  console.log('formName', form.name.value);
+  console.log('formEmail', form.email.value);
+  console.log('formEmail', form.password.value);
+
+  sendHttpRequest('POST', 'http://localhost:3001/users/signup', {
+    name: form.name.value,
+    email: form.email.value,
+    password: form.password.value,//sadece sayı olunca işe yaramıyor..
+  }).then(responseData => {
+    console.log("response data: ", responseData);
+  }).catch(err => {
+    console.log(err);
+  });
+};
+
+// const login = () => {
+//   const form = document.querySelector('form');
+//   console.log('formEmail', form.email.value);
+//   console.log('formPassword', form.password.value);
+
+//   sendHttpRequest('GET', 'http://localhost:3001/users/login', {
+//     email: form.email.value,
+//     password: form.password.value
+//   }).then(responseData => {
+//     console.log("response data: ",responseData);
+//   }).catch(err => {
+//     console.log("error : ", err);
+//   });
+// };
+
+if (registerbtn) {
+  registerbtn.addEventListener("click", register);
+}
+if (loginbtn) {
+  loginbtn.addEventListener("click", login);
+}
+
